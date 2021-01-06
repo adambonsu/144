d2b3e011919f75e62f87cee6b6407941302246e9
Author Name <adam@bonsu.io>
2021-01-06 21:07:35 +0000

    initial spec/144/test_spec.rb

---
+++ spec/144/test_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module OneFourFour
+  describe Test do
+    describe "#start" do
+      before(:each) do
+        @game = 'indigo'
+        @output = double('output').as_null_object
+        @questions = ['5x5']
+        @test = Test.new(@game, @questions, @output)
+      end
+      it 'sends a welcome message' do
+        expect(@output).to receive(:puts).with('Welcome to 144')
+        @test.start
+      end
+      it 'provides the first question, prompting for an answer' do
+        expect(@output).to receive(:puts).with("#{@questions.first}:")
+        @test.start
+      end
+    end
+  end
+end
